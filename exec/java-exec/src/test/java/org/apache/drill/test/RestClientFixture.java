94ee7619e8fe7fa708117073f5999bf696a836a3
Timothy Farkas <timothyfarkas@apache.org>
2017-09-17 12:49:04 -0700

    - debug

--- exec/java-exec/src/test/java/org/apache/drill/test/RestClientFixture.java
+++ exec/java-exec/src/test/java/org/apache/drill/test/RestClientFixture.java
@@ -71,6 +71,8 @@ public class RestClientFixture implements AutoCloseable {
    * @return All the external options
    */
   public List<StatusResources.OptionWrapper> getStatusOptions() {
+    System.out.println(baseTarget.path(StatusResources.PATH_OPTIONS_JSON)
+      .request(MediaType.APPLICATION_JSON).get().readEntity(String.class));
     return baseTarget.path(StatusResources.PATH_OPTIONS_JSON)
       .request(MediaType.APPLICATION_JSON)
       .get(new GenericType<List<StatusResources.OptionWrapper>>() {});
